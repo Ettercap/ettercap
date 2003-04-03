45b9974e6fd23e82bde433c3fbf96576f1adec10
alor <alor>
2003-04-03 20:12:36 +0000

    sync includes

--- include/el_functions.h
+++ include/el_functions.h
@@ -37,7 +37,10 @@ extern int is_conn(struct log_header_packet *pck, int *versus);
 /* el_target */
 extern void target_compile(char *target);
 extern int is_target_pck(struct log_header_packet *pck);
-extern int is_target_info(struct log_header_info *pck);
+extern int is_target_info(struct log_header_info *inf);
+
+/* el_profiles */
+extern int profile_add_info(struct log_header_info *inf);
 
 #endif
 

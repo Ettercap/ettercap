63e4f9c9cc1fb62c4352310ecf8b5c99d018e971
alor <alor>
2003-06-14 09:30:01 +0000

    new icmp decoder

---
+++ src/protocols/ec_icmp.c
@@ -0,0 +1,113 @@
+/*
+    ettercap -- ICMP decoder module
+
+    Copyright (C) ALoR & NaGA
+    
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+    $Header: /home/drizzt/dev/sources/ettercap.cvs/ettercap_ng/src/protocols/ec_icmp.c,v 1.1 2003/06/14 09:30:01 alor Exp $
+*/
+
+#include <ec.h>
+#include <ec_decode.h>
+
+/* globals */
+
+struct icmp_header {
+   u_int8   type;     /* message type */
+   u_int8   code;     /* type sub-code */
+   u_int16  checksum;
+   union {
+      struct {
+         u_int16  id;
+         u_int16  sequence;
+      } echo;       /* echo datagram */
+      u_int32     gateway; /* gateway address */
+      struct {
+         u_int16  unused;
+         u_int16  mtu;
+      } frag;       /* path mtu discovery */
+   } un;
+};
+
+
+/* protos */
+
+FUNC_DECODER(decode_icmp);
+void icmp_init(void);
+
+/*******************************************/
+
+/*
+ * this function is the initializer.
+ * it adds the entry in the table of registered decoder
+ */
+
+void __init icmp_init(void)
+{
+   add_decoder(PROTO_LAYER, NL_TYPE_ICMP, decode_icmp);
+}
+
+
+FUNC_DECODER(decode_icmp)
+{
+   struct icmp_header *icmp;
+
+   icmp = (struct icmp_header *)DECODE_DATA;
+  
+   DECODED_LEN = sizeof(struct icmp_header);
+
+   /* fill the data */
+   PACKET->L4.len = DECODED_LEN;
+   PACKET->L4.header = (u_char *)DECODE_DATA;
+   PACKET->L4.options = NULL;
+   
+   PACKET->L4.proto = htons(NL_TYPE_ICMP);
+   
+   /* this is a lie... but we have to put this somewhere */
+   PACKET->L4.flags = icmp->type;
+  
+   /* XXX - checksum check */
+   
+   /* 
+    * HOOK POINT:  PACKET_ICMP 
+    */
+   hook_point(PACKET_ICMP, po);
+  
+   /* 
+    * if the host is sending strange 
+    * ICMP messages, it might be a router
+    */
+   switch (icmp->type) {
+      case ICMP_DEST_UNREACH:
+         switch (icmp->code) {
+            case ICMP_NET_UNREACH:
+            case ICMP_HOST_UNREACH:
+               PACKET->PASSIVE.flags |= FP_ROUTER;
+               break;
+         }
+      case ICMP_REDIRECT:
+      case ICMP_TIME_EXCEEDED:
+         PACKET->PASSIVE.flags |= FP_ROUTER;
+         break;
+   }
+   
+   return NULL;
+}
+
+/* EOF */
+
+// vim:ts=3:expandtab
+

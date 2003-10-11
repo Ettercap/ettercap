41b2472e37d7b19ce5f79bec47f168812e511ff1
alor <alor>
2003-10-11 19:44:39 +0000

    interface reorganization

---
+++ src/interfaces/text/ec_text_conn.c
@@ -0,0 +1,111 @@
+/*
+    ettercap -- diplay the connection list 
+
+    Copyright (C) ALoR & NaGA
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+    $Id: ec_text_conn.c,v 1.1 2003/10/11 19:44:39 alor Exp $
+*/
+
+#include <ec.h>
+#include <ec_threads.h>
+#include <ec_poll.h>
+#include <ec_interfaces.h>
+#include <ec_conntrack.h>
+#include <ec_inet.h>
+#include <ec_proto.h>
+
+/* globals */
+
+/* proto */
+
+void text_connections(void);
+void conn_print(int n, struct conn_object *co);
+
+/*******************************************/
+
+void text_connections(void)
+{   
+   fprintf(stdout, "\nConnections list:\n\n");
+  
+   /* call the callbacking function */
+   conntrack_print(1, INT_MAX, &conn_print); 
+
+   fprintf(stdout, "\n");
+}
+
+/*
+ * prints a connection
+ */
+void conn_print(int n, struct conn_object *co)
+{
+   char tmp[MAX_ASCII_ADDR_LEN];
+
+   fprintf(stdout, "%3d)  ", n);
+   switch (co->L4_proto) {
+      case NL_TYPE_UDP:
+         fprintf(stdout, "U ");
+         break;
+      case NL_TYPE_TCP:
+         fprintf(stdout, "T ");
+         break;
+      default:
+         fprintf(stdout, "  ");
+         break;
+   }
+   fprintf(stdout, "%15s:%-5d -- ", ip_addr_ntoa(&co->L3_addr1, tmp), ntohs(co->L4_addr1));   
+   fprintf(stdout, "%15s:%-5d ", ip_addr_ntoa(&co->L3_addr2, tmp), ntohs(co->L4_addr2));   
+   switch (co->status) {
+      case CONN_IDLE:
+         fprintf(stdout, "IDLE    ");
+         break;
+      case CONN_OPENING:
+         fprintf(stdout, "OPENING ");
+         break;
+      case CONN_OPEN:
+         fprintf(stdout, "OPEN    ");
+         break;
+      case CONN_ACTIVE:
+         fprintf(stdout, "ACTIVE  ");
+         break;
+      case CONN_CLOSING:
+         fprintf(stdout, "CLOSING ");
+         break;
+      case CONN_CLOSED:
+         fprintf(stdout, "CLOSED  ");
+         break;
+      case CONN_KILLED:
+         fprintf(stdout, "KILLED  ");
+         break;
+   }
+   fprintf(stdout, "  TX: %d", co->xferred);
+
+   if (co->DISSECTOR.user) {
+      fprintf(stdout, "\n\t\tUSER: %s", co->DISSECTOR.user);
+      fprintf(stdout, "\n\t\tPASS: %s", co->DISSECTOR.pass);
+      if (co->DISSECTOR.info)
+         fprintf(stdout, "\n\t\tINFO: %s", co->DISSECTOR.info);
+   }
+   
+   fprintf(stdout, "\n");
+
+}
+
+
+/* EOF */
+
+// vim:ts=3:expandtab
+
---
+++ src/interfaces/text/ec_text_plugin.c
@@ -0,0 +1,152 @@
+/*
+    ettercap -- text GUI for plugin management
+
+    Copyright (C) ALoR & NaGA
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+    $Id: ec_text_plugin.c,v 1.1 2003/10/11 19:44:39 alor Exp $
+*/
+
+#include <ec.h>
+#include <ec_poll.h>
+#include <ec_ui.h>
+#include <ec_threads.h>
+#include <ec_plugins.h>
+
+/* proto */
+
+int text_plugin(char *plugin);
+void text_plugin_list(char active, struct plugin_ops *ops);
+
+/*******************************************/
+
+
+/* the interface */
+
+int text_plugin(char *plugin)
+{
+   int type;
+
+   DEBUG_MSG("text_interface");
+   
+   /*
+    * if the plugin name is "list", print the 
+    * plugin list and exit
+    */
+   if (!strcasecmp(plugin, "list")) {
+      /* delete any previous message */
+      ui_msg_purge_all();
+
+      USER_MSG("\nAvailable plugins :\n\n");
+      type = plugin_list_print(PLP_MIN, PLP_MAX, &text_plugin_list);
+      if (type == -ENOTFOUND) {
+         USER_MSG("No plugin found !\n\n");
+         return -ENOTFOUND;
+      }
+      
+      USER_MSG("\n\n");
+      /* 
+       * return an error, so the text interface 
+       * ends and returns to main
+       */
+      return -EINVALID;
+   }
+   
+   /* what type of plugin are we loading ? */
+   type = plugin_get_type(plugin);
+
+   if (type == -ENOTFOUND)
+      FATAL_MSG("%s plugin can not be found !", plugin);
+   
+   if (plugin_is_activated(plugin) == 0)
+      USER_MSG("Activating %s plugin [%s]...\n", (type == PL_HOOK) ? "hook" : "standalone" , plugin);
+   else
+      USER_MSG("Deactivating %s plugin [%s]...\n", (type == PL_HOOK) ? "hook" : "standalone" , plugin);
+  
+   switch(type) {
+      case PL_HOOK:
+         if (plugin_is_activated(plugin) == 1)
+            plugin_fini(plugin);
+         else
+            plugin_init(plugin);
+         USER_MSG("Done.\n\n");
+         /* return success so the text interface remains active */
+         return ESUCCESS;
+         break;
+      case PL_STANDALONE:
+         /*
+          * pay attention on this !
+          * if the plugin init does not return,
+          * we are blocked here. So it is encouraged
+          * to write plugins which spawn a thread
+          * and immediately return
+          */
+         plugin_init(plugin);
+         USER_MSG("Done.\n\n");
+         break;
+   }
+
+   
+   LOOP {
+   
+      CANCELLATION_POINT();
+      
+      /* if there is a pending char to be read */
+      if ( ec_poll_read(fileno(stdin), 1) ) {
+         
+         char ch = 0;
+         ch = getchar();
+         switch(ch) {
+            case 'H':
+            case 'h':
+               USER_MSG("\n Plugin [%s] is running...\n\n", plugin);
+               USER_MSG("  [PLUGINS] Inline help:\n\n");
+               USER_MSG("   [qQ]  - quit (terminate the plugin)\n\n");
+               break;
+            
+            case 'Q':
+            case 'q':
+               /* finalize the plugin and exit */
+               plugin_fini(plugin);
+               return -EINVALID;
+               break;
+         }
+                                                                           
+      }
+
+      /* print pending USER_MSG messages */
+      ui_msg_flush(10);
+                                 
+   }
+  
+   /* NOT REACHED */
+   
+}
+
+/*
+ * callback function for displaying the plugin list 
+ */
+void text_plugin_list(char active, struct plugin_ops *ops)
+{
+   USER_MSG("[%d][%10s] %15s %4s  %s\n", active, 
+         (ops->type == PL_HOOK) ? "hook" : "standalone",
+         ops->name, ops->version, ops->info);  
+}
+
+/* EOF */
+
+// vim:ts=3:expandtab
+

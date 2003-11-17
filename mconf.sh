dcf207ccac125fad7c89e43497a8e0b3d9a37a02
alor <alor>
2003-11-17 10:58:23 +0000

    autotools update

---
+++ mconf.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo
+
+if test ! -f ./configure; then
+   echo "ERROR: configure not found..."
+   echo "Running autogen.sh to generate the autoscripts"
+   if test ! -f ./autogen.sh; then
+      echo "FATAL: autogen.sh not found."
+      exit
+   fi
+   ./autogen.sh
+fi
+
+echo "Configuring ettercap for maintainers mode..."
+echo 
+./configure --enable-debug --enable-maintainer-mode $* || exit 
+
+echo
+echo "Making ettercap to be tested in the current directory"
+echo 
+make test

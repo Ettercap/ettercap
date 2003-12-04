576bf913b19870fdf1d3b4a09a7f83ab572c9064
lordnaga <lordnaga>
2003-12-04 14:35:21 +0000

    Just a reminder

--- src/protocols/ec_ppp.c
+++ src/protocols/ec_ppp.c
@@ -17,13 +17,14 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-    $Id: ec_ppp.c,v 1.4 2003/12/03 14:47:54 lordnaga Exp $
+    $Id: ec_ppp.c,v 1.5 2003/12/04 14:35:21 lordnaga Exp $
 */
 
 #include <ec.h>
 #include <ec_decode.h>
 #include <ec_dissect.h>
 
+/* XXX - conditional use of openssl library */
 #include <openssl/sha.h>
 
 

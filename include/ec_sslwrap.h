5f51656f415c7f1ff7d09a93395663727f04f9b8
lordnaga <lordnaga>
2004-03-08 12:37:26 +0000

    Some fix

--- include/ec_sslwrap.h
+++ include/ec_sslwrap.h
@@ -1,13 +1,14 @@
 
-/* $Id: ec_sslwrap.h,v 1.1 2004/03/08 12:12:49 lordnaga Exp $ */
+/* $Id: ec_sslwrap.h,v 1.2 2004/03/08 12:37:26 lordnaga Exp $ */
 
 #ifndef EC_SSLWRAP_H
 #define EC_SSLWRAP_H
 
 #include <ec_decode.h>
+#include <ec_threads.h>
 
 extern void sslw_dissect_add(char *name, u_int32 port, FUNC_DECODER_PTR(decoder), u_char status);
-extern void sslw_start(void);
+extern EC_THREAD_FUNC(sslw_start);
 
 #define SSL_ENABLED 1
 #define SSL_DISABLED 0

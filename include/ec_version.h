10ef416a6f6f1d7415c4b3e16e379d5c56a7ab30
alor <alor>
2004-04-15 20:57:32 +0000

    just release pre1, now it's time for pre2

--- include/ec_version.h
+++ include/ec_version.h
@@ -1,10 +1,10 @@
 
-/* $Id: ec_version.h,v 1.9 2004/04/04 14:27:31 alor Exp $ */
+/* $Id: ec_version.h,v 1.10 2004/04/15 20:57:32 alor Exp $ */
 
 #ifndef EC_VERS_H
 #define EC_VERS_H
 
-#define EC_VERSION     "NG-0.7.0_pre1"
+#define EC_VERSION     "NG-0.7.0_pre2"
 #define EC_PROGRAM     "ettercap"
 #define EC_COPYRIGHT   "2001-2004"
 #define EC_AUTHORS     "ALoR & NaGA"

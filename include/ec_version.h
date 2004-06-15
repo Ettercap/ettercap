279d691e8206a2700d3a87801c1be43092094f1e
alor <alor>
2004-06-15 08:15:34 +0000

    back to development

--- include/ec_version.h
+++ include/ec_version.h
@@ -1,10 +1,10 @@
 
-/* $Id: ec_version.h,v 1.11 2004/05/17 08:43:34 alor Exp $ */
+/* $Id: ec_version.h,v 1.12 2004/06/15 08:15:34 alor Exp $ */
 
 #ifndef EC_VERS_H
 #define EC_VERS_H
 
-#define EC_VERSION     "NG-0.7.0_rc1"
+#define EC_VERSION     "NG-0.7.0_rc2"
 #define EC_PROGRAM     "ettercap"
 #define EC_COPYRIGHT   "2001-2004"
 #define EC_AUTHORS     "ALoR & NaGA"

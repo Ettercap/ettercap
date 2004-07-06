cf0197b6787844586a65a301ded8c459eae8a196
alor <alor>
2004-07-06 10:56:23 +0000

    added the isolate plugin

--- include/ec_version.h
+++ include/ec_version.h
@@ -1,10 +1,10 @@
 
-/* $Id: ec_version.h,v 1.13 2004/07/05 19:26:17 alor Exp $ */
+/* $Id: ec_version.h,v 1.14 2004/07/06 10:56:23 alor Exp $ */
 
 #ifndef EC_VERS_H
 #define EC_VERS_H
 
-#define EC_VERSION     "NG-0.7.0"
+#define EC_VERSION     "NG-0.7.1_cvs"
 #define EC_PROGRAM     "ettercap"
 #define EC_COPYRIGHT   "2001-2004"
 #define EC_AUTHORS     "ALoR & NaGA"

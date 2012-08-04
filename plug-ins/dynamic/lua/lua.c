62af4c695d99d53cb50fcc6a0f1034cf0d87e067
Mike Ryan <falter@gmail.com>
2012-08-04 15:18:13 -0500

    Now generating and using SWIG headers. SWIG doesn't seem to like constants defined via enum.

--- plug-ins/dynamic/lua/lua.c
+++ plug-ins/dynamic/lua/lua.c
@@ -26,6 +26,8 @@
 #include <ec_plugins.h>                /* required for plugin ops */
 #include <ec_hook.h>
 
+#include "lua_swig.h"
+
 #include "lua.h"
 #include "lualib.h"
 #include "lauxlib.h"
@@ -48,9 +50,11 @@ extern int luaopen_ettercap(lua_State* L);
 static int dynamic_lua_init(void *);
 static int dynamic_lua_fini(void *);
 static void dynamic_lua_handle_dns(struct packet_object *);
+static void dynamic_lua_handle_http(struct packet_object *);
+static void dynamic_lua_handle_eth(struct packet_object *);
 
 lua_State* _lua_state;
-
+swig_type_info* _p_swigt__p_packet_object;
 
 /* plugin operations */
 
@@ -105,6 +109,17 @@ static int dynamic_lua_init(void *dynamic_lua)
     /* load lua libraries */
     luaL_openlibs(_lua_state);
     luaopen_ettercap(_lua_state);
+
+    /* get pointers to all the different types that we'll be casting from/to */
+    _p_swigt__p_packet_object = SWIG_MangledTypeQuery(_lua_state, "_p_packet_object");
+    // Create a lua-style version of the object.
+    if(_p_swigt__p_packet_object) {
+      USER_MSG("WOOO, Got the proper type.\n");
+    } else {
+      USER_MSG("No dice.\n");
+    }
+  
+    /* Now load the lua files */
     int dofile = luaL_dofile(_lua_state, "hello.lua");
 
     if (dofile == 0) {
@@ -117,6 +132,8 @@ static int dynamic_lua_init(void *dynamic_lua)
     }
 
     hook_add(HOOK_PROTO_DNS, &dynamic_lua_handle_dns);
+    hook_add(HOOK_PROTO_HTTP, &dynamic_lua_handle_http);
+    hook_add(HOOK_PACKET_ETH, &dynamic_lua_handle_eth);
    /* return PLUGIN_FINISHED if the plugin has terminated
     * its execution.
     * return PLUGIN_RUNNING if it has spawned a thread or it
@@ -137,11 +154,14 @@ static int dynamic_lua_fini(void *dynamic_lua)
     */
     USER_MSG("DYNAMIC_LUA: plugin finalization\n");
     hook_del(HOOK_PROTO_DNS, &dynamic_lua_handle_dns);
+    hook_del(HOOK_PROTO_HTTP, &dynamic_lua_handle_http);
+    hook_del(HOOK_PACKET_ETH, &dynamic_lua_handle_eth);
     /* cleanup Lua */
     if (_lua_state) {
       lua_close(_lua_state);
-      _lua_state = NULL;
     }
+    _lua_state = NULL;
+    _p_swigt__p_packet_object = NULL;
     return PLUGIN_FINISHED;
 }
 
@@ -149,7 +169,29 @@ static void dynamic_lua_handle_dns(struct packet_object *po)
 {
     if (_lua_state) {
       lua_getglobal(_lua_state,"handle_dns_packet");
-      lua_call(_lua_state,0,0);
+
+      SWIG_NewPointerObj(_lua_state, (void *) po, _p_swigt__p_packet_object, 0);
+      lua_call(_lua_state,1,0);
+    }
+}
+
+static void dynamic_lua_handle_http(struct packet_object *po)
+{
+    if (_lua_state) {
+      lua_getglobal(_lua_state,"handle_http_packet");
+
+      SWIG_NewPointerObj(_lua_state, (void *) po, _p_swigt__p_packet_object, 0);
+      lua_call(_lua_state,1,0);
+    }
+}
+
+static void dynamic_lua_handle_eth(struct packet_object *po)
+{
+    if (_lua_state) {
+      lua_getglobal(_lua_state,"handle_eth_packet");
+
+      SWIG_NewPointerObj(_lua_state, (void *) po, _p_swigt__p_packet_object, 0);
+      lua_call(_lua_state,1,0);
     }
 }
 

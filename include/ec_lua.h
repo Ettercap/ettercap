c1cf22a5b1127359dd6f90546a315e68e9b2816f
sussurro <sussurro@happypacket.net>
2012-09-15 19:08:50 -0700

    fixed ifdefs for already loaded check

--- include/ec_lua.h
+++ include/ec_lua.h
@@ -1,7 +1,5 @@
-
-#ifndef EC_LOG_H
-#define EC_LOG_H
-
+#ifndef EC_LUA_H
+#define EC_LUA_H
 EC_API_EXTERN int ec_lua_init();
 EC_API_EXTERN int ec_lua_fini();
 EC_API_EXTERN int ec_lua_load_script(const char * name);

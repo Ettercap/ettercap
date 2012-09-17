8d8d11cf19beceda9d4f109219d7114db8a792b9
Mike Ryan <falter@gmail.com>
2012-09-16 22:21:02 -0500

    No more Luajit ffi callbacks!
    - Removed our usage of the Luajir FFI callback stuff: it has bugs and is
      slow.
    
    - Now dispatching packets directly to Lua from C. This is faster, and
      doesn't crash all the time.

--- share/lua/core/ec_funcs.lua
+++ share/lua/core/ec_funcs.lua
@@ -2,6 +2,7 @@ module('ec_funcs', package.seeall)
 Ettercap.ec_hooks = {}
 Ettercap.scripts = {}
 
+local ettercap = require("ettercap")
 local Script = require("ec_script")
 
 -- Simple logging function that maps directly to ui_msg
@@ -11,22 +12,21 @@ end
 
 -- This is the cleanup function that gets called.
 Ettercap.cleanup = function() 
-  Ettercap.log("Cleaning up lua hooks!!\n")
-  for key, hook in pairs(Ettercap.ec_hooks) do
-    Ettercap.log("Cleaning up a lua hook...\n")
-    Ettercap.ffi.C.hook_del(hook[1], hook[2])
-    -- Free the callback ?
-  end
 end
 
 Ettercap.hook_add = function (point, func)
-  Ettercap.log("1")
-  func_cb = Ettercap.ffi.cast("hook_cb_func", func)
-  Ettercap.log("2")
-  Ettercap.ffi.C.hook_add(point, func_cb)
-  Ettercap.log("3")
-  table.insert(Ettercap.ec_hooks, {point, func_cb})
-  Ettercap.log("4")
+  ettercap.hook_add(point)
+  table.insert(Ettercap.ec_hooks, {point, func})
+end
+
+Ettercap.dispatch_hook = function (point, po)
+  -- We cast the packet into the struct that we want, and then hand it off.
+  local s_po = Ettercap.ffi.cast("struct packet_object *", po);
+  for key, hook in pairs(Ettercap.ec_hooks) do
+    if hook[1] == point then
+      hook[2](s_po)
+    end
+  end
 end
 
 Ettercap.load_script = function (name, args)

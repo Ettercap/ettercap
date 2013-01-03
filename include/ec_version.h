512ea94b6c12474a3e3d8dcf4f345493688288f5
Emilio Escobar <eescobar@gmail.com>
2013-01-02 23:56:19 -0500

    Updating version and copyright year

--- include/ec_version.h
+++ include/ec_version.h
@@ -3,12 +3,12 @@
 #ifndef EC_VERS_H
 #define EC_VERS_H
 
-#define EC_VERSION            "0.7.5"
+#define EC_VERSION            "0.7.5.1"
 #define EC_VERSION_MAJOR      0
 #define EC_VERSION_MINOR      7
 #define EC_VERSION_REVISION   5 
 #define EC_PROGRAM            "ettercap"
-#define EC_COPYRIGHT          "2001-2012"
+#define EC_COPYRIGHT          "2001-2013"
 #define EC_AUTHORS            "Ettercap Development Team"
 
 #endif

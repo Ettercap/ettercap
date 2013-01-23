90ccc5319bfbc3e84b0221df101b3ccc38c13ba9
Gianfranco Costamagna <costamagnagianfranco@yahoo.it>
2013-01-24 00:11:18 +0100

    Deleted little space

--- src/ec_scan.c
+++ src/ec_scan.c
@@ -122,7 +122,7 @@ void build_hosts_list(void)
       scan_thread(NULL);
    else 
       /* do the scan in a separate thread */
-      ec_thread_new("scan", "scanning thread", &scan_thread, NULL);
+      ec_thread_new("scan", "scanning thread", &ecan_thread, NULL);
 #endif
 }
 
@@ -144,7 +144,7 @@ static EC_THREAD_FUNC(scan_thread)
    ts.tv_nsec = 0;
 
    /* in text mode and demonized this function should NOT be a thread */
-   if (GBL_UI->type == UI_TEXT || GBL_UI->type == UI_DAEMONIZE || GBL_UI->type == UI_CURSES )
+   if (GBL_UI->type == UI_TEXT || GBL_UI->type == UI_DAEMONIZE || GBL_UI->type == UI_CURSES)
       threadize = 0;
 
 #ifdef OS_MINGW

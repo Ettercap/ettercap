45f5c916c57f5d5d864e15cf74ec0a41e6212c82
Emilio Escobar <eescobar@gmail.com>
2013-01-29 14:36:20 -0500

    Updating version to 0.7.5.2

--- include/ec_version.h
+++ include/ec_version.h
@@ -3,7 +3,7 @@
 #ifndef EC_VERS_H
 #define EC_VERS_H
 
-#define EC_VERSION            "0.7.5.1"
+#define EC_VERSION            "0.7.5.2"
 #define EC_VERSION_MAJOR      0
 #define EC_VERSION_MINOR      7
 #define EC_VERSION_REVISION   5 

8c1672f667c6e59b52a25f75cff7ad0586e7ec5b
alor <alberto.ornaghi@gmail.com>
2013-03-27 17:16:52 +0100

    Revert "register ip over pppoe"
    This reverts commit 5f4de4bc6925632121e9f3c93eeabecd18bc7eeb.

--- src/protocols/ec_ip.c
+++ src/protocols/ec_ip.c
@@ -91,7 +91,6 @@ void __init ip_init(void)
 {
    add_decoder(NET_LAYER, LL_TYPE_IP, decode_ip);
    add_decoder(PROTO_LAYER, NL_TYPE_IPIP, decode_ip);
-   add_decoder(NET_LAYER, LL_TYPE_PPP_IP, decode_ip);
    add_injector(CHAIN_LINKED, IP_MAGIC, inject_ip);
    add_injector(CHAIN_LINKED, STATELESS_IP_MAGIC, stateless_ip);
 }

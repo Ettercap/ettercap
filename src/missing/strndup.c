8fc7141bdf1b8535c9b7ea6fbeb5a17f0f741e1a
Gianfranco Costamagna <costamagnagianfranco@yahoo.it>
2013-04-19 17:21:11 +0200

    Forgot missing header

--- src/missing/strndup.c
+++ src/missing/strndup.c
@@ -18,6 +18,8 @@ License along with libiberty; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
 Boston, MA 02110-1301, USA.  */
 
+#include <missing/strndup.h>
+
 char *
 strndup (const char *s, size_t n)
 {

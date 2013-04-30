781f2617573e3f1139b9d5f8edc08093b2490991
AntonioCollarino <anto.collarino@gmail.com>
2013-04-30 22:24:54 +0200

    fixed a part of the issue #214 and added right copyright in plugins.

--- plug-ins/autoadd/autoadd.c
+++ plug-ins/autoadd/autoadd.c
@@ -1,7 +1,7 @@
 /*
     autoadd -- ettercap plugin -- Report suspicious ARP activity
 
-    Copyright (C) ALoR & NaGA
+    Copyright (C) Ettercap Dev. Team
     
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
--- plug-ins/reply_arp/reply_arp.c
+++ plug-ins/reply_arp/reply_arp.c
@@ -1,7 +1,7 @@
 /*
     reply_arp -- ettercap plugin -- Simple arp responder
 
-    Copyright (C) ALoR & NaGA
+    Copyright (C) Ettercap Dev. Team
     
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by

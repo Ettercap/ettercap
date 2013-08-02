f61434db9fd1bcc5c28c113fd3c8042def44f595
Alexander Koeppe <format_c@online.de>
2013-08-02 22:54:05 +0200

    detect router capabilities from NADV messages

--- src/protocols/ec_icmp6.c
+++ src/protocols/ec_icmp6.c
@@ -72,6 +72,12 @@ FUNC_DECODER(decode_icmp6)
       case ICMP6_PKT_TOO_BIG:
          PACKET->PASSIVE.flags |= FP_ROUTER;
          break;
+      case ICMP6_NEIGH_ADV:
+         if ((*((u_int8*)icmp6 + 4) & 0x80) == 0x80) {
+            /* Router flag set in neighbor advertisement */
+            PACKET->PASSIVE.flags |= FP_ROUTER;
+         }
+         break;
    }
 
    hook_point(HOOK_PACKET_ICMP6, po);

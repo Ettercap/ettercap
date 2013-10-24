7e947ea26795bdaa30679f6e190cfad3ac8707d7
Dhiru Kholia <dhiru@openwall.com>
2013-10-24 15:03:41 +0530

    add support for parsing OSPFv2 MD5 authentication packets

--- src/dissectors/ec_ospf.c
+++ src/dissectors/ec_ospf.c
@@ -2,6 +2,7 @@
     ettercap -- dissector ospf -- works over IP !
 
     Copyright (C) ALoR & NaGA
+    Copyright (C) Dhiru Kholia (dhiru [at] openwall.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -21,7 +22,7 @@
 
 /*
  * RFC: 2328
- * 
+ *
  *      0                   1                   2                   3
  *       0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
  *      +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -39,14 +40,15 @@
  *      +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
  *
 */
- 
+
 #include <ec.h>
 #include <ec_decode.h>
 #include <ec_dissect.h>
 
-#define OSPF_AUTH_LEN   8
-#define OSPF_AUTH       1
-#define OSPF_NO_AUTH    0
+#define OSPF_AUTH_LEN            8
+#define OSPF_NO_AUTH             0
+#define OSPF_AUTH                1  // OSPF_AUTH_SIMPLE
+#define OSPF_AUTH_CRYPTOGRAPHIC  2
 
 struct ospf_hdr {
    u_int8   ver;
@@ -58,6 +60,39 @@ struct ospf_hdr {
    u_int16  auth_type;
    u_int32  auth1;
    u_int32  auth2;
+};
+
+static char itoa16[16] =  "0123456789abcdef";
+
+static inline void hex_encode(unsigned char *str, int len, unsigned char *out)
+{
+   int i;
+   for (i = 0; i < len; ++i) {
+      out[0] = itoa16[str[i]>>4];
+      out[1] = itoa16[str[i]&0xF];
+      out += 2;
+   }
+}
+
+/* borrowed from GNU Zebra project */
+#define BUFSIZE                  2048 /* big enough for OSPF */
+#define OSPF_HEADER_SIZE         24U
+#define OSPF_AUTH_SIMPLE_SIZE     8U
+#define OSPF_AUTH_MD5_SIZE       16U
+
+struct ospf_header
+{
+   u_int8 version;                       /* OSPF Version. */
+   u_int8 type;                          /* Packet Type. */
+   u_int16_t length;                     /* Packet Length. */
+   struct in_addr router_id;             /* Router ID. */
+   struct in_addr area_id;               /* Area ID. */
+   u_int16_t checksum;                   /* Check Sum. */
+   u_int16_t auth_type;                  /* Authentication Type. */
+   u_int16_t zero;                       /* Should be 0. */
+   u_int8 key_id;                        /* Key ID. */
+   u_int8 auth_data_len;                 /* Auth Data Length. */
+   u_int32_t crypt_seqnum;               /* Cryptographic Sequence Number. */
 };
 
 /* protos */
@@ -77,7 +112,7 @@ void __init ospf_init(void)
    dissect_add("ospf", PROTO_LAYER, NL_TYPE_OSPF, dissector_ospf);
 }
 
-/* 
+/*
  * the passwords collected by ospf will not be logged
  * in logfile since it is not over TCP or UDP.
  * anyway we can print them in the user message window
@@ -85,48 +120,75 @@ void __init ospf_init(void)
 
 FUNC_DECODER(dissector_ospf)
 {
-   DECLARE_DISP_PTR_END(ptr, end);
+   // DECLARE_DISP_PTR_END(ptr, end);  // this is broken!
    struct ospf_hdr *ohdr;
+   u_int8 *ptr = buf;
    char tmp[MAX_ASCII_ADDR_LEN];
    char pass[12];
 
    /* don't complain about unused var */
-   (void)end;
+   // (void)end;
 
    /* skip empty packets */
-   if (PACKET->DATA.len == 0)
+   // if (PACKET->DATA.len == 0) {  // this is broken!
+   if (buflen == 0) {
       return NULL;
+   }
 
    DEBUG_MSG("OSPF --> dissector_ospf");
-  
+
    ohdr = (struct ospf_hdr *)ptr;
-   
+
    /* authentication */
-   if ( ntohs(ohdr->auth_type) == OSPF_AUTH ) {
-      
+   if ( ntohs(ohdr->auth_type) == OSPF_AUTH_CRYPTOGRAPHIC ) {
+        struct ospf_header *oh = (struct ospf_header *)ptr;
+        unsigned char buf1[BUFSIZE] = { 0 };
+        unsigned char buf2[BUFSIZE] = { 0 };
+
+        int length = ntohs(ohdr->len);
+
+        if (oh->auth_data_len != OSPF_AUTH_MD5_SIZE) {
+                return NULL;
+        }
+
+        /* validate the packet */
+        if (length * 2 > BUFSIZE)
+                return NULL;
+        if (length > buflen)
+                return NULL;
+
+        hex_encode(ptr, length, buf1);
+        hex_encode(ptr + length, OSPF_AUTH_MD5_SIZE, buf2);
+
+        DISSECT_MSG("OSPF-%s-%d:$netmd5$%s$%s\n",
+                ip_addr_ntoa(&PACKET->L3.dst, tmp),
+                ntohs(PACKET->L4.dst),
+                buf1, buf2);
+   } else if ( ntohs(ohdr->auth_type) == OSPF_AUTH ) {
       DEBUG_MSG("\tDissector_ospf PASS");
-      
-      /* 
-       * we use a local variable since this does 
+
+      /*
+       * we use a local variable since this does
        * not need to reach the top half
        */
       char o[OSPF_AUTH_LEN];
       snprintf(o, OSPF_AUTH_LEN, "%u", ohdr->auth1);
       strncpy(pass, o, OSPF_AUTH_LEN);
-      
-   } 
+
+      DISSECT_MSG("OSPF : %s:%d -> AUTH: %s \n", ip_addr_ntoa(&PACKET->L3.dst, tmp),
+                ntohs(PACKET->L4.dst),
+                pass);
+   }
 
    /* no authentication */
-   if ( ntohs(ohdr->auth_type) == OSPF_NO_AUTH ) {
-      
+   else if ( ntohs(ohdr->auth_type) == OSPF_NO_AUTH ) {
       DEBUG_MSG("\tDissector_ospf NO AUTH");
-      
       strncpy(pass, "No Auth", 7);
+
+      DISSECT_MSG("OSPF : %s:%d -> AUTH: %s \n", ip_addr_ntoa(&PACKET->L3.dst, tmp),
+                ntohs(PACKET->L4.dst),
+                pass);
    }
-   
-   DISSECT_MSG("OSPF : %s:%d -> AUTH: %s \n", ip_addr_ntoa(&PACKET->L3.dst, tmp),
-                                             ntohs(PACKET->L4.dst), 
-                                             pass);
 
    return NULL;
 }

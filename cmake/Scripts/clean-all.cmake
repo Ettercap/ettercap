2c0987b7b14850841af44911ec8dc8d8d57a2fa7
Gianfranco Costamagna <costamagnagianfranco@yahoo.it>
2014-01-18 11:18:53 +0100

    Added an unistall target, and tweaked clean-all script to clean also the directories

--- cmake/Scripts/clean-all.cmake
+++ cmake/Scripts/clean-all.cmake
@@ -5,6 +5,17 @@ set(cmake_generated ${CMAKE_BINARY_DIR}/CMakeCache.txt
                     ${CMAKE_BINARY_DIR}/cmake_install.cmake  
                     ${CMAKE_BINARY_DIR}/Makefile
                     ${CMAKE_BINARY_DIR}/CMakeFiles
+                    ${CMAKE_BINARY_DIR}/cmake_uninstall.cmake
+                    ${CMAKE_BINARY_DIR}/CTestTestfile.cmake
+                    ${CMAKE_BINARY_DIR}/install_manifest.txt
+                    ${CMAKE_BINARY_DIR}/desktop/
+                    ${CMAKE_BINARY_DIR}/include/
+                    ${CMAKE_BINARY_DIR}/man/
+                    ${CMAKE_BINARY_DIR}/plug-ins/
+                    ${CMAKE_BINARY_DIR}/share/
+                    ${CMAKE_BINARY_DIR}/src/
+                    ${CMAKE_BINARY_DIR}/tests/
+                    ${CMAKE_BINARY_DIR}/utils/
 )
 
 foreach(file ${cmake_generated})

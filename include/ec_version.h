845f299de87ef2eadf2707b4840840104679d2cf
Gianfranco Costamagna <costamagnagianfranco@yahoo.it>
2015-03-14 13:28:26 +0100

    Update changelog for 0.8.2

--- include/ec_version.h
+++ include/ec_version.h
@@ -1,12 +1,12 @@
 #ifndef ETTERCAP_VERS_H
 #define ETTERCAP_VERS_H
 
-#define EC_VERSION            "0.8.1"
+#define EC_VERSION            "0.8.2"
 #define EC_VERSION_MAJOR      0
 #define EC_VERSION_MINOR      8
-#define EC_VERSION_REVISION   1
+#define EC_VERSION_REVISION   2
 #define EC_PROGRAM            "ettercap"
-#define EC_COPYRIGHT          "2001-2014"
+#define EC_COPYRIGHT          "2001-2015"
 #define EC_AUTHORS            "Ettercap Development Team"
 
 #endif

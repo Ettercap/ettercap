15fdd5267b19084ca8e81cdd91c9a51c0bfc2123
Gianfranco Costamagna <costamagnagianfranco@yahoo.it>
2018-03-11 17:44:52 +0100

    Simplify #includes and #defines in ef.h and el.h

--- include/ef.h
+++ include/ef.h
@@ -1,57 +1,7 @@
 #ifndef ETTERFILTER_H
 #define ETTERFILTER_H
 
-#include <config.h>
-
-#include <sys/types.h>
-#include <sys/time.h>
-
-#include <stdio.h>
-#include <stdlib.h>
-#if !defined (__USE_GNU)  /* for memmem(), strsignal(), etc etc... */
-   #define __USE_GNU
-#endif
-#include <string.h>
-#if defined (__USE_GNU)
-   #undef __USE_GNU
-#endif
-#include <strings.h>
-#include <unistd.h>
-#include <time.h>
-
-#ifndef HAVE_STRSEP
-   #include <missing/strsep.h>
-#endif
-
-#ifdef OS_WINDOWS
-   #include <windows.h>
-#endif
-
-#include <ec_queue.h>
-#include <ec_stdint.h>
-#include <ec_error.h>
-#include <ec_strings.h>
-
-#define SAFE_CALLOC(x, n, s) do { \
-   x = calloc(n, s); \
-   ON_ERROR(x, NULL, "virtual memory exhausted"); \
-} while(0)
-
-#define SAFE_REALLOC(x, s) do { \
-   x = realloc(x, s); \
-   ON_ERROR(x, NULL, "virtual memory exhausted"); \
-} while(0)
-
-#define SAFE_FREE(x) do{ if(x) { free(x); x = NULL; } }while(0)
-
-#define __init __attribute__ ((constructor))
-
-#define LOOP for(;;)
-
-/* file operations */ 
-#ifndef OS_WINDOWS
-   #define O_BINARY  0
-#endif
+#include <ec.h>
 
 struct ef_globals {
    char *source_file;

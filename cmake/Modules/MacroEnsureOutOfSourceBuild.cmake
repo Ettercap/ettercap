d2a68376373b7a5939bcab2b749d703a674cfcae
Ali Abdulkadir <autostart.ini@gmail.com>
2018-04-11 05:15:16 +0300

    Simplified macro_ensure_out_of_source_build()
    It has one job. It can do that in its own .cmake.
    Come to think of it: Why are we enforcing an out of source build anyways?
    This should be optional...

--- cmake/Modules/MacroEnsureOutOfSourceBuild.cmake
+++ cmake/Modules/MacroEnsureOutOfSourceBuild.cmake
@@ -6,11 +6,13 @@
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-macro(MACRO_ENSURE_OUT_OF_SOURCE_BUILD _errorMessage)
+macro(MACRO_ENSURE_OUT_OF_SOURCE_BUILD)
 
   string(COMPARE EQUAL "${CMAKE_SOURCE_DIR}" "${CMAKE_BINARY_DIR}" _insource)
   if(_insource)
-    message(SEND_ERROR "${_errorMessage}")
+    message(SEND_ERROR "${PROJECT_NAME} requires an out of source build.
+Please create a separate build directory and run \
+'cmake /path/to/${PROJECT_NAME} [options]' there.")
     message(FATAL_ERROR
 "Remove the file CMakeCache.txt in ${CMAKE_SOURCE_DIR} first."
     )

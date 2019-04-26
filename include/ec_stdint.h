bad4b68e6ac73d0cc3dc67622fd9393e792d78ca
Alexander Koeppe <alexander@koeppe.rocks>
2019-04-26 08:16:41 +0200

    switch to C99 data types

--- include/ec_stdint.h
+++ include/ec_stdint.h
@@ -34,10 +34,10 @@
 	typedef int32_t   int32;
 	typedef int64_t   int64;
 
-	typedef u_int8_t   u_int8;
-	typedef u_int16_t  u_int16;
-	typedef u_int32_t  u_int32;
-	typedef u_int64_t  u_int64;
+	typedef uint8_t   u_int8;
+	typedef uint16_t  u_int16;
+	typedef uint32_t  u_int32;
+	typedef uint64_t  u_int64;
 
    #if defined OS_BSD_OPEN && !defined HAVE_STDINT_H
       #define INT8_MAX     CHAR_MAX

95208f4863e50675aed3223bd4c49667446b30ba
Alexander Koeppe <format_c@online.de>
2020-05-05 20:12:58 +0200

    Release candidate version numbering

--- include/ec_version.h
+++ include/ec_version.h
@@ -1,10 +1,10 @@
 #ifndef ETTERCAP_VERS_H
 #define ETTERCAP_VERS_H
 
-#define EC_VERSION            "0.8.3"
+#define EC_VERSION            "0.8.4-rc"
 #define EC_VERSION_MAJOR      0
 #define EC_VERSION_MINOR      8
-#define EC_VERSION_REVISION   3
+#define EC_VERSION_REVISION   4
 #ifndef PROGRAM
    #define PROGRAM            "ettercap"
 #endif

105b3cb550058c0a5c71a9837cae74b186666f00
Alexander Koeppe <format_c@online.de>
2020-08-03 20:12:04 +0200

    Set 0.8.4-rc as candidate version for next minor release

--- include/ec_version.h
+++ include/ec_version.h
@@ -1,11 +1,11 @@
 #ifndef ETTERCAP_VERS_H
 #define ETTERCAP_VERS_H
 
-#define EC_VERSION            "0.8.3.1"
+#define EC_VERSION            "0.8.4-rc"
 #define EC_VERSION_MAJOR       0
 #define EC_VERSION_MINOR       8
-#define EC_VERSION_REVISION    3
-#define EC_VERSION_SUBREVISION 1
+#define EC_VERSION_REVISION    4
+#define EC_VERSION_SUBREVISION 0
 #ifndef PROGRAM
    #define PROGRAM            "ettercap"
 #endif

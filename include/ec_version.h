19b888611e211bdade79ed3d140c02f0e70e2c2a
Gianfranco Costamagna <locutusofborg@debian.org>
2026-04-07 21:22:42 +0200

    Update version and release number, prepare for 0.8.4.1

--- include/ec_version.h
+++ include/ec_version.h
@@ -1,11 +1,11 @@
 #ifndef ETTERCAP_VERS_H
 #define ETTERCAP_VERS_H
 
-#define EC_VERSION            "0.8.4"
+#define EC_VERSION            "0.8.4.1"
 #define EC_VERSION_MAJOR       0
 #define EC_VERSION_MINOR       8
 #define EC_VERSION_REVISION    4
-#define EC_VERSION_SUBREVISION 0
+#define EC_VERSION_SUBREVISION 1
 #ifndef PROGRAM
    #define PROGRAM            "ettercap"
 #endif
